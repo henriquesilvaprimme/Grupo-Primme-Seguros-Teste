a9e9d0ca86b2908004632dd5fad823f51c4c3a5c
henriquesilvaprimme <henriquesilva.primme@gmail.com>
2025-07-08 13:02:25 -0300

    Update api.jsx

--- src/config/api.jsx
+++ src/config/api.jsx
@@ -3,17 +3,17 @@ const BASE_GAS_URL = 'https://script.google.com/macros/s/AKfycby8vujvd5ybEpkaZ0k
 
 // Objeto contendo todas as URLs específicas do seu Google Apps Script
 export const API_ENDPOINTS = {
-  BASE_GAS_URL: 'SUA_NOVA_URL_GAS_AQUI', // Atualize esta linha
-  GET_LEADS: 'SUA_NOVA_URL_GAS_AQUI?v=getLeads',
-  GET_LEADS_FECHADOS: 'SUA_NOVA_URL_GAS_AQUI?v=pegar_clientes_fechados',
-  GET_USUARIOS: 'SUA_NOVA_URL_GAS_AQUI?v=pegar_usuario',
-  POST_ALTERAR_STATUS: 'SUA_NOVA_URL_GAS_AQUI?v=alterar_status',
-  POST_ALTERAR_ATRIBUIDO: 'SUA_NOVA_URL_GAS_AQUI?v=alterar_atribuido',
-  POST_ALTERAR_SEGURADORA: 'SUA_NOVA_URL_GAS_AQUI?v=alterar_seguradora',
-  POST_CRIAR_USUARIO: 'SUA_NOVA_URL_GAS_AQUI?v=criar_usuario',
-  POST_ALTERAR_USUARIO: 'SUA_NOVA_URL_GAS_AQUI?v=alterar_usuario',
-  LISTAR_NOMES_USUARIOS: 'SUA_NOVA_URL_GAS_AQUI?v=listar_nomes_usuarios',
-  POST_CRIAR_LEAD: 'SUA_NOVA_URL_GAS_AQUI?v=criar_lead', // Adicione esta linha
+  BASE_GAS_URL: 'https://script.google.com/macros/s/AKfycby8vujvd5ybEpkaZ0kwZecAWOdaL0XJR84oKJBAIR9dVYeTCv7iSdTdHQWBb7YCp349/exec', // Atualize esta linha
+  GET_LEADS: 'https://script.google.com/macros/s/AKfycby8vujvd5ybEpkaZ0kwZecAWOdaL0XJR84oKJBAIR9dVYeTCv7iSdTdHQWBb7YCp349/exec?v=getLeads',
+  GET_LEADS_FECHADOS: 'https://script.google.com/macros/s/AKfycby8vujvd5ybEpkaZ0kwZecAWOdaL0XJR84oKJBAIR9dVYeTCv7iSdTdHQWBb7YCp349/exec?v=pegar_clientes_fechados',
+  GET_USUARIOS: 'https://script.google.com/macros/s/AKfycby8vujvd5ybEpkaZ0kwZecAWOdaL0XJR84oKJBAIR9dVYeTCv7iSdTdHQWBb7YCp349/exec?v=pegar_usuario',
+  POST_ALTERAR_STATUS: 'https://script.google.com/macros/s/AKfycby8vujvd5ybEpkaZ0kwZecAWOdaL0XJR84oKJBAIR9dVYeTCv7iSdTdHQWBb7YCp349/exec?v=alterar_status',
+  POST_ALTERAR_ATRIBUIDO: 'https://script.google.com/macros/s/AKfycby8vujvd5ybEpkaZ0kwZecAWOdaL0XJR84oKJBAIR9dVYeTCv7iSdTdHQWBb7YCp349/exec?v=alterar_atribuido',
+  POST_ALTERAR_SEGURADORA: 'https://script.google.com/macros/s/AKfycby8vujvd5ybEpkaZ0kwZecAWOdaL0XJR84oKJBAIR9dVYeTCv7iSdTdHQWBb7YCp349/exec?v=alterar_seguradora',
+  POST_CRIAR_USUARIO: 'https://script.google.com/macros/s/AKfycby8vujvd5ybEpkaZ0kwZecAWOdaL0XJR84oKJBAIR9dVYeTCv7iSdTdHQWBb7YCp349/exec?v=criar_usuario',
+  POST_ALTERAR_USUARIO: 'https://script.google.com/macros/s/AKfycby8vujvd5ybEpkaZ0kwZecAWOdaL0XJR84oKJBAIR9dVYeTCv7iSdTdHQWBb7YCp349/exec?v=alterar_usuario',
+  LISTAR_NOMES_USUARIOS: 'https://script.google.com/macros/s/AKfycby8vujvd5ybEpkaZ0kwZecAWOdaL0XJR84oKJBAIR9dVYeTCv7iSdTdHQWBb7YCp349/exec?v=listar_nomes_usuarios',
+  POST_CRIAR_LEAD: 'https://script.google.com/macros/s/AKfycby8vujvd5ybEpkaZ0kwZecAWOdaL0XJR84oKJBAIR9dVYeTCv7iSdTdHQWBb7YCp349/exec?v=criar_lead', // Adicione esta linha
 };
 
 // Se você precisar da base URL em algum lugar, pode exportá-la também
