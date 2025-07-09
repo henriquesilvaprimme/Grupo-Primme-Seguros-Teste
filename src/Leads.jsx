c1a12d22d85b2a0ad18dc678a3306cb427833a28
henriquesilvaprimme <henriquesilva.primme@gmail.com>
2025-07-09 04:07:28 -0300

    Update Leads.jsx

--- src/Leads.jsx
+++ src/Leads.jsx
@@ -185,7 +185,7 @@ const Leads = ({ leads, usuarios, onUpdateStatus, transferirLead, usuarioLogado,
       {isLoading && (
         <div className="absolute inset-0 bg-white flex justify-center items-center z-10"> {/* Mantido 'absolute' */}
           <div className="animate-spin rounded-full h-20 w-20 border-t-2 border-b-2 border-indigo-500"></div>
-          <p className="ml-4 text-lg text-gray-700">Carregando leads...</p>
+          <p className="ml-4 text-lg text-gray-700">Carregando LEADS...</p>
         </div>
       )}
 
