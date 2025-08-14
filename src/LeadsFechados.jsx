75e23ce833110da11e0c54b5a902079a7dd4db79
henriquesilvaprimme <henriquesilva.primme@gmail.com>
2025-08-13 21:26:08 -0300

    Update LeadsFechados.jsx

--- src/LeadsFechados.jsx
+++ src/LeadsFechados.jsx
@@ -354,8 +354,10 @@ const LeadsFechados = ({ leads, usuarios, onUpdateInsurer, onConfirmInsurer, onU
     <div style={{ padding: '20px', position: 'relative' }}>
       {isLoading && (
         <div className="absolute inset-0 bg-white flex justify-center items-center z-10" style={{ opacity: 0.8 }}>
-          <div className="animate-spin rounded-full h-20 w-20 border-t-2 border-b-2 border-indigo-500"></div>
-          <p className="ml-4 text-lg text-gray-700">Carregando LEADS FECHADOS...</p>
+          <div style={{ position: 'relative', top: '-25vh' }} className="flex flex-col items-center">
+            <div className="animate-spin rounded-full h-20 w-20 border-t-2 border-b-2 border-indigo-500"></div>
+            <p className="mt-4 text-lg text-gray-700">Carregando LEADS FECHADOS...</p>
+          </div>
         </div>
       )}
 
